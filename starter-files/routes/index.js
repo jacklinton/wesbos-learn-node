1dab932e12afc7fa06ed6f1f30b4fd1248154bce
Jack Linton <jacklinton@mac.com>
2018-10-09 22:07:30 -0400

    All work up to flash usage

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require("../controllers/storeController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore));
+
+// Reverse name param and print on screen
+router.get('/reverse/:name', ( req, res ) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
