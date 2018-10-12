58677b044cbac9ae3049915e3e0da44c10c814ff
Jack Linton <jacklinton@mac.com>
2018-10-12 16:15:55 -0400

    Complete address & coordinates

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -3,26 +3,46 @@ mongoose.Promise = global.Promise;
 const slug = require("slugs");
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Please enter a store name!"
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a store name!"
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: "Point"
     },
-    slug: String,
-    description: {
-        type: String,
-        trim: true
-    },
-    tags: [String]
+    coordinates: [
+      {
+        type: Number,
+        required: "You must supply coordinates!"
+      }
+    ],
+    address: {
+      type: String,
+      required: "You must include an address!"
+    }
+  }
 });
 
 storeSchema.pre("save", function(next) {
-    if (!this.isModified("name")){
-        next();
-    }
-    this.slug = slug(this.name);
+  if (!this.isModified("name")) {
     next();
-    // TODO make more resilient so slugs are unique
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO make more resilient so slugs are unique
 });
 
 module.exports = mongoose.model("Store", storeSchema);
